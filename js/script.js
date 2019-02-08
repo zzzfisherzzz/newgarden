831879ffd6d097b873a6c3e05fac4d8c62efa922
Artem <zzzfisherzzz@bigmir.net>
2019-02-08 12:52:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,181 @@
+var HIDDEN_CLASS_NAME = 'hidden'
+var TARGET_CLASS_NAME = 'target'
+var SOURCE_CLASS_NAME = 'source'
+
+var targetIdToShow = 1
+
+function main() {
+	var targets = getElements(TARGET_CLASS_NAME)
+	var sources = getElements(SOURCE_CLASS_NAME)
+	sources.forEach(function (sourceNode) {
+		var sourceNodeId = extractId(sourceNode, SOURCE_CLASS_NAME)
+		sourceNode.addEventListener('mouseover' , function () {
+			showTarget(targets, sourceNodeId)
+		})
+	})
+	showTarget(targets, targetIdToShow)
+}
+
+function getElements(type) {
+	return [].slice.call(document.querySelectorAll('.' + type)).sort(function (targetNode1, targetNode2) {
+		var target1Num = extractId(targetNode1, TARGET_CLASS_NAME)
+		var target2Num = extractId(targetNode2, TARGET_CLASS_NAME)
+		return target1Num > target2Num
+	})
+}
+
+function extractId(targetNode, baseClass) {
+	var currentClassIndex = targetNode.classList.length
+	while (currentClassIndex--) {
+		var currentClass = targetNode.classList.item(currentClassIndex)
+		var maybeIdNum = parseInt(currentClass.split('-')[1])
+		if (isNaN(maybeIdNum)) {
+			continue
+		}
+		var classStrinToValidate = baseClass + '-' + maybeIdNum
+		if (classStrinToValidate === currentClass) {
+			return maybeIdNum
+		}
+	}
+}
+
+function showTarget(targets, targetId) {
+	targets.forEach(function (targetNode, targetIndex) {
+    var currentTargetNodeId = extractId(targetNode, TARGET_CLASS_NAME)
+		if (currentTargetNodeId === targetId) {
+			targetNode.classList.remove(HIDDEN_CLASS_NAME)
+		} else {
+			targetNode.classList.add(HIDDEN_CLASS_NAME)
+		}
+	})
+}
+
+main();
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  dots: false,
+  focusOnSelect: true
+});
+
+
+
+//*MEnu *//
+
+$(function(){
+
+	$('.menu-toggle').click(function(){
+		$(this).toggleClass('active')
+		$('.menu').slideToggle(400)
+	})
+
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
+
+	$('.banner-slider, .testimonial-slider').slick({
+		arrows: false,
+		dots: true,
+	});
+
+	$('.portfolio-slider').slick({
+		dots: true,
+		appendArrows: '.portfolio-slider__buttons',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+		responsive: [
+	    {
+	      breakpoint: 767,
+	      settings: {
+	      	dots: false,
+	      }
+	    }
+	   
+	  ] 
+	});
+
+
+	$nav_tabs_slider = $('.nav-tab-list');
+	settings = {
+		slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+		infinite: false,
+	}
+
+	$nav_tabs_slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).find('.slick-current').siblings().removeClass('active');
+		var id = $(this).find('.slick-current a').attr('href');
+		$(id).removeClass('hide');
+		$(this).find('.slick-current').addClass('active');
+		return false
+	})
+
+
+	$(window).on('resize load', function(){
+		if($(window).width() > 767) {
+			if($nav_tabs_slider.hasClass('slick-initialized')) {
+				$nav_tabs_slider.slick('unslick')
+			}
+			return
+		}
+		if(!$nav_tabs_slider.hasClass('slick-initialized')) {
+			return $nav_tabs_slider.slick(settings)
+		}
+	})
+
+});
+
+
+  $(document).ready(function () {
+        $(".grey-line, #home").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top - 180;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+
+$(document).ready(function () {
+	$("#home").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top ;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
+$('.home-slider').slick({
+	dots: true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
